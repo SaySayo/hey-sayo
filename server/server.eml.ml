353de0e158fadecff1f85eab89e5547df8c713fe
saysayo <sayobamigbade@gmail.com>
2022-09-26 08:41:23 +0100

    improve server

--- server/server.eml.ml
+++ server/server.eml.ml
@@ -10,58 +10,99 @@ let home =
     </head>
     <body>
       <header>
-        <h1>Welcome To aHref Chat Group</h1>
+        <h1>Welcome To aHrefs Group Chat</h1>
         <p>Pick up from where you left off...</p>
-        <div><span>Please Enter Your Name: </span> <span id="username" contenteditable="true">User</span><i class="fa fa-pencil"></i></div>
       </header>
-      <section class="chatBlock">
-        <div class="chatRoom" id="chatRoom">
-          <ol class="chatList" id="chatList">
-            <li><em>Bot: </em><span>Hello</span></li>
-          </ol>
-        </div>
-        <div class="chatInput">
-          <form>
-            <input id="chatInput" type="text" placeholder="Type here..." />
-            <button type="submit" class="icons plane" id="chatSubmit">
-              <i class="fa fa-send"></i>
-             </button>
-          </form>
-        </div>
-      </section>
+      <main>
+        <section class="chat-block">
+          <div class="username">
+            <span class="username-desc">Username: </span> 
+            <span class="username-title" id="username-title" contenteditable=true title="Click to edit">Click To Edit</span>
+          </div>
+          <div class="chat-room" id="chatRoom">
+            <ol class="chat-list" id="chatList">
+              <li>
+                <span>
+                  <span class="chat-sender">Bot: </span>
+                  <span class="chat-msg">Hello there, you are very welcome! </span>
+                </span>
+                <i></i>
+              </li>
+            </ol>
+          </div>
+          <div class="chatInput">
+            <form>
+              <input id="chatInput" type="text" placeholder="Type here..." />
+              <button type="submit" class="icons plane" id="chatSubmit">
+                <i class="fa fa-send"></i>
+              </button>
+            </form>
+          </div>
+        </section>
+      </main>
 
       <script>
-        let username = document.getElementById("username");
-        let message = document.getElementById("chatInput");
+        let username = document.getElementById("username-title");
         let chatList = document.getElementById("chatList");
+        let messageElem = document.getElementById("chatInput");
+
+        const createAndAddChat = (msg) => {
+          let li = document.createElement("li");
+              let chatWrapper = document.createElement("span");
+              let chatSender = document.createElement("span");
+              let chatMsg = document.createElement("span");
+              let chatFeedback = document.createElement("i");
+              chatSender.textContent = `${msg.sender}: `;
+              chatMsg.textContent = msg.message;
+              chatWrapper.appendChild(chatSender);
+              chatWrapper.appendChild(chatMsg);
+              chatSender.classList.add("chat-sender");
+              chatMsg.classList.add("chat-msg");
+              li.appendChild(chatWrapper);
+              li.appendChild(chatFeedback);
+              chatList.appendChild(li);
+        }
+
         let socket = new WebSocket("ws://" + window.location.host + "/websocket");
 
         socket.onmessage = function (event) {
-          alert(event.data);
-          let li = document.createElement("li");
-          let em = document.createElement("em");
-          let span = document.createElement("span");
-          em.textContent = `${username.textContent}: `;
-          li.appendChild(em);
-          span.textContent = event.data;
-          li.appendChild(span);
-          chatList.appendChild(li);
+          let messages = document.querySelectorAll(".chat-msg");
+          let exist = false;
+          let recievedData = JSON.parse(event.data);
+          messages.forEach((message, i) => {
+            if(message.innerHTML.toLowerCase() === recievedData.message.toLowerCase()) {
+              message.parentElement.nextElementSibling.classList.add("fa", "fa-check", "sent");
+              exist = true;
+            } 
+          })
+
+          if (exist === false) {
+            createAndAddChat(recievedData);
+          }
         };
 
-        document.querySelector("form").onsubmit = function () {
+        document.querySelector("form").onsubmit = function (e) {
+          e.preventDefault();
+          let message = messageElem.value;
           if (socket.readyState != WebSocket.OPEN)
             return false;
-          if (!message.value)
+          if (!message)
             return false;
 
-          socket.send(message.value);
-          message.value = "";
+          let msg = {
+            sender: username.textContent,
+            message
+          }
+
+          createAndAddChat(msg);
+
+          socket.send(JSON.stringify(msg));
+          messageElem.value = "";
           return false;
         };
       </script>
     </body>
   </html>
-
 
 let clients : (int, Dream.websocket) Hashtbl.t =
   Hashtbl.create 5
@@ -81,18 +122,13 @@ let send message =
   |> List.of_seq
   |> Lwt_list.iter_p (fun client -> Dream.send client message)
 
-  (* val get_time () *)
-(* let receive client = Dream.receive client
-
-let now = (new%js Js.date_now)##getTime  *)
-
 let handle_client client =
   let client_id = track client in
   let rec loop () =
     match%lwt Dream.receive client with
     | Some message ->
+      Dream.log "Server recieved a message: %s" (message);
       let%lwt () = send message in
-      let%lwt () = send "Message Received" in
       loop ()
     | None ->
       forget client_id;
